4ccef676038bb1521896b42a2cb8bb7306e06920
Nick Baker <ndbaker1@outlook.com>
2021-01-19 11:45:17 -0600

    state saved in appdata

--- master.py
+++ master.py
@@ -1,11 +1,14 @@
 import re
 import os
+import json
 from glob import glob
 from PyQt5.QtWidgets import *
 
 from masterList import MasterList
 from controls import ListControls
 from playlistView import PlaylistView
+
+APPDATA_FOLDER = 'PlaylistSorter'
 
 
 class MasterWidget(QSplitter):
@@ -19,8 +22,19 @@ class MasterWidget(QSplitter):
             self.masterList
         )
 
+        self.initConfig()
+
         self.addWidget(self.masterList)
         self.addWidget(self.listControls)
+        for playlistFile in self.config['openPlaylists']:
+            playlist = PlaylistView(
+                playlistFile,
+                self.masterList.removeSongs
+            )
+            self.addWidget(playlist)
+            self.playlistWidgets.append(playlist)
+            self.masterList.loadSongs()
+        self.masterList.loadSongs()
 
     def addPlaylist(self):
         playlistFile = QFileDialog.getOpenFileName(self, "Select Playlist")[0]
@@ -33,7 +47,37 @@ class MasterWidget(QSplitter):
         self.addWidget(playlist)
         self.playlistWidgets.append(playlist)
         self.masterList.loadSongs()
+        self.config['openPlaylists'].append(playlistFile)
+        self.writeConfig()
 
     def updateSongDirectory(self):
         self.masterList.songDirectory = str(QFileDialog.getExistingDirectory(self, "Select Directory")) + '/'
+        self.config['songDirectory'] = self.masterList.songDirectory
+        self.writeConfig()
         self.masterList.loadSongs()
+
+    #########################################################
+    #   checks for config file and creates one if not found
+    #########################################################
+    def initConfig(self):
+        appdataPath = os.getenv('APPDATA').replace('\\', '/')
+        appdataPath += '/' + APPDATA_FOLDER
+        os.makedirs(appdataPath, exist_ok=True)
+
+        self.configFilePath = appdataPath + '/config.json'
+
+        if os.path.exists(self.configFilePath):
+            with open(self.configFilePath, 'r') as configFile:
+                self.config = json.load(configFile)
+                self.masterList.songDirectory = self.config['songDirectory']
+        else:
+            with open(self.configFilePath, 'w') as configFile:
+                self.config = {
+                    'songDirectory': '',
+                    'openPlaylists': []
+                }
+                self.writeConfig()
+
+    def writeConfig(self):
+        with open(self.configFilePath, 'w') as configFile:
+            json.dump(self.config, configFile)
