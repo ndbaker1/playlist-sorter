81a703ac08e6e119feb295dc28b8e335879f4487
Nick Baker <ndbaker1@outlook.com>
2021-01-20 02:11:12 -0600

    made icon path a variable

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from PyQt5.QtGui import QIcon
 from master import MasterWidget
 
 APP_NAME = "Playlist Set Manager"
+ICON_PATH = 'icon.ico'
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, app_name):
+    def __init__(self, app_name, icon_path):
         super(MainWindow, self).__init__()
         self.setWindowTitle(app_name)
-        self.setWindowIcon(QIcon('icon.ico'))
+        self.setWindowIcon(QIcon(icon_path))
         self.setCentralWidget(MasterWidget(app_name))
         self.resize(1280, 720)
         self.show()
@@ -18,5 +19,5 @@ class MainWindow(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow(APP_NAME)
+    window = MainWindow(APP_NAME, ICON_PATH)
     sys.exit(app.exec_())
